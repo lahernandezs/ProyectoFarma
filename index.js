8c5a9c35df2a79d8951f5c600dba2f34f7e712e3
Luis Hernandez <lahernandezs@MacBook-Pro-de-Luis.local>
2018-02-19 10:12:47 -0300

    commit auth google

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app=express();
 
 //para que passport entienda la estrategia. Las estrategias es el detalle de un loggin en particular
 //por ejemplo, google, fb, etc.s
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},(accessToken,refreshToken,profile,done)=>{
+    console.log('access Token ',accessToken);
+    console.log('refresh Token',refreshToken);
+    console.log('profile',profile);
+}));
 
-app.post('/auth/google',(req,res)=>{
-    res.send(req);
-})
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}));
+app.get('/auth/google/callback/',passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
