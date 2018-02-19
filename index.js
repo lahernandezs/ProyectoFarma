a06f06567cad4c318440aca3e9034d33e155557e
Luis Hernandez <lahernandezs@MacBook-Pro-de-Luis.local>
2018-02-19 10:51:17 -0300

    Test

--- index.js
+++ index.js
@@ -11,16 +11,19 @@ passport.use(new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL:'/auth/google/callback'
-},(accessToken,refreshToken,profile,done)=>{
+},(accessToken)=>{
     console.log('access Token ',accessToken);
-    console.log('refresh Token',refreshToken);
-    console.log('profile',profile);
+  // console.log('refresh Token',refreshToken);
+    //console.log('profile',profile);
 }));
 
 app.get('/auth/google',passport.authenticate('google',{
     scope:['profile','email']
 }));
-app.get('/auth/google/callback/',passport.authenticate('google'));
 
+//app.get('/auth/google/callback/',passport.authenticate('google'));
+app.get('/auth/google/callback',(req,res)=>{
+    console.log(req);
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
