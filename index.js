31fd28282a4aed9fdbe547603b275ec395548898
Luis Hernandez <lahernandezs@MacBook-Pro-de-Luis.local>
2018-02-19 01:14:37 -0300

    se agrega nuevos cambios

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send({hi:'Nuevo'});
-});
-app.post('/hola',(req,res)=>{
+//para que passport entienda la estrategia. Las estrategias es el detalle de un loggin en particular
+//por ejemplo, google, fb, etc.s
+passport.use(new GoogleStrategy());
+
+//clientId 253347017840-eo8luvt84nu56vt8ga6o6j8cqaec5mct.apps.googleusercontent.com
+//Secret 4701deFWyJoGFcbaeecK_2rU
+
+app.post('/auth/google',(req,res)=>{
     res.send(req);
 })
 
